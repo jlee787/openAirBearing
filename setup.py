825febec10aae362467098231f1a767f6da3ef2d
Mikael Miettinen <mikael.miettinen@iki.fi>
2025-04-01 15:17:54 +0300

    added requiremets.txt

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "dash>=2.0.0,<3.0.0",
         "numpy>=1.21.0,<2.0.0",
-        "plotly>=5.0.0,<6.0.0",
+        "plotly>=6.0.0,<7.0.0",
         "scipy>=1.7.0,<2.0.0",
     ],
     classifiers=[
