e09be472ad00a7046eab492ad6a696e18f9c24eb
Mikael Miettinen <mikael.miettinen@iki.fi>
2025-04-02 20:07:26 +0300

    update example simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -1,20 +1,22 @@
-import matplotlib.pyplot as plt
 import openairbearing as ab
-
 
 if __name__ == "__main__":
 
-    bearing = ab.CircularBearing()
+    # initialize circular bearing with parameters
+    bearing = ab.CircularBearing(
+        xa=40, Qsc=5, nx=50, nh=60, error_type="quadratic", error=-2e-6
+    )
+
+    # solve with analytic and numeric methods
     result = [
         ab.solve_bearing(bearing, ab.ANALYTIC),
         ab.solve_bearing(bearing, ab.NUMERIC),
     ]
+
+    # plot bearing shape
+    figure = ab.plot_bearing_shape(bearing)
+    figure.show()
+
+    # plot results
     figure = ab.plot_key_results(bearing, result)
     figure.show()
-    # bearing = RectangularBearing(nx=5, ny=7, error_type="quadratic", error=1e-6)
-    # b = bearing
-    # result = solve_bearing(b, NUMERIC)
-
-    # figure = plots.plot_key_results(b, result)
-    # # figure = plots.plot_bearing_shape(bearing)
-    # figure.show()
